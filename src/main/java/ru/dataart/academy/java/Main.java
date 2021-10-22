a2a313b7cd42024f68e2998e34d27705c5ed00c5
CodeDestroy <andrei_novichihin@mail.ru>
2021-10-22 16:59:58 +0300

    commit

--- src/main/java/ru/dataart/academy/java/Main.java
+++ src/main/java/ru/dataart/academy/java/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Calculator get number of 1: " +
-                calculator.getNumberOfChar("/home/andrey/vrn-java-06/vrn-java-06/src/main/resources/test.zip", '1'));
+                calculator.getNumberOfChar("src/main/resources/test.zip", '1'));
         System.out.println("Calculator get max length: " +
-                calculator.getMaxWordLength("/home/andrey/vrn-java-06/vrn-java-06/src/main/resources/test1.zip"));
+                calculator.getMaxWordLength("src/main/resources/test1.zip"));
     }
 }
